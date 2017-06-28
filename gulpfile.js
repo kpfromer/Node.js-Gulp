1737bbfefeb0769a737170d0f496cd492e0509ab
Kyle Pfromer <kpfromer2@gmail.com>
2017-06-28 14:30:33 -0600

    gulp can concat files!

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 "use strict";
 
-const gulp = require("gulp");
+const gulp = require("gulp"),
+    concat = require("gulp-concat");
 
-gulp.task("hello", function () {
-    console.log("Hello, World");
-});
-
-gulp.task("secondTask", function () {
-    console.log("I should be run second!");
+gulp.task("concatScripts", function () {
+    //order matters!
+    //gulp.src = readable stream of data (a node thing)
+    gulp.src(["js/jquery.js",
+        "js/sticky/jquery.sticky.js",
+        "js/main.js"])
+        .pipe(concat("app.js"))//concats the readable stream of data into file app.js
+        .pipe(gulp.dest("js"));//sends app.js to js folder
 });
 
 // second param is the dependencies (things to run before running task)
